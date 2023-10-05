c1a389168625dbe2c29f18eac0664fe4bce0e781
levy <heylevy@gmail.com>
2023-10-04 23:59:37 -0400

    d3 direct page

---
+++ gwidgs/d3/pageplain.js
@@ -0,0 +1,137 @@
+// let's assume that it's imported in an html file
+// var data1;
+
+//registering code to run when a document is ready
+function ready(fn) {
+  if (document.readyState !== 'loading') {
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+// when a document is ready, register the calendar and subscribe to grist events
+ready(async () => {
+  document.getElementById('container').append(initD3b());
+});
+
+function initD3b() {
+  const format = d3.format(",");
+  const nodeSize = 17;
+  var data1 = getTreeData1();
+  const root = d3.hierarchy(data1).eachBefore((i => d => d.index = i++)(0));
+  const nodes = root.descendants();
+  const width = 928;
+  const height = (nodes.length + 1) * nodeSize;
+
+  const columns = [
+    {
+      label: "Size", 
+      value: d => d.value, 
+      format, 
+      x: 280
+    },
+    {
+      label: "Count", 
+      value: d => d.children ? 0 : 1, 
+      format: (value, d) => d.children ? format(value) : "-", 
+      x: 340
+    }
+  ];
+
+  const svg = d3.create("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("viewBox", [-nodeSize / 2, -nodeSize * 3 / 2, width, height])
+      .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif; overflow: visible;");
+
+  const link = svg.append("g")
+      .attr("fill", "none")
+      .attr("stroke", "#999")
+    .selectAll()
+    .data(root.links())
+    .join("path")
+      .attr("d", d => `
+        M${d.source.depth * nodeSize},${d.source.index * nodeSize}
+        V${d.target.index * nodeSize}
+        h${nodeSize}
+      `);
+
+  const node = svg.append("g")
+    .selectAll()
+    .data(nodes)
+    .join("g")
+      .attr("transform", d => `translate(0,${d.index * nodeSize})`);
+
+  node.append("circle")
+      .attr("cx", d => d.depth * nodeSize)
+      .attr("r", 2.5)
+      .attr("fill", d => d.children ? null : "#999");
+
+  node.append("text")
+      .attr("dy", "0.32em")
+      .attr("x", d => d.depth * nodeSize + 6)
+      .text(d => d.data.name);
+
+  node.append("title")
+      .text(d => d.ancestors().reverse().map(d => d.data.name).join("/"));
+
+  for (const {label, value, format, x} of columns) {
+    svg.append("text")
+        .attr("dy", "0.32em")
+        .attr("y", -nodeSize)
+        .attr("x", x)
+        .attr("text-anchor", "end")
+        .attr("font-weight", "bold")
+        .text(label);
+
+    node.append("text")
+        .attr("dy", "0.32em")
+        .attr("x", x)
+        .attr("text-anchor", "end")
+        .attr("fill", d => d.children ? null : "#555")
+      .data(root.copy().sum(value).descendants())
+        .text(d => format(d.value, d));
+  }
+  return svg.node();
+}
+
+
+function initD3a() {
+  // Declare the chart dimensions and margins.
+  const width = 640;
+  const height = 400;
+  const marginTop = 20;
+  const marginRight = 20;
+  const marginBottom = 30;
+  const marginLeft = 40;
+
+  // Declare the x (horizontal position) scale.
+  const x = d3.scaleUtc()
+      .domain([new Date("2023-01-01"), new Date("2024-01-01")])
+      .range([marginLeft, width - marginRight]);
+
+  // Declare the y (vertical position) scale.
+  const y = d3.scaleLinear()
+      .domain([0, 100])
+      .range([height - marginBottom, marginTop]);
+
+  // Create the SVG container.
+  const svg = d3.create("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+  // Add the x-axis.
+  svg.append("g")
+      .attr("transform", `translate(0,${height - marginBottom})`)
+      .call(d3.axisBottom(x));
+
+  // Add the y-axis.
+  svg.append("g")
+      .attr("transform", `translate(${marginLeft},0)`)
+      .call(d3.axisLeft(y));
+
+  // Append the SVG element.
+  return svg.node();
+
+}
