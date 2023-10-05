a7d30bd7b9db216765fc063366c605ad824f7ff7
levy <heylevy@gmail.com>
2023-10-05 00:49:53 -0400

    D3c collapsible tree

--- gwidgs/d3/flare-2.js
+++ gwidgs/d3/flare-2.js
@@ -1,3 +1,273 @@
+var data1 = getTreeData2();
+
+function initD3c() {
+
+    // Specify the charts’ dimensions. The height is variable, depending on the layout.
+    const width = 928;
+    const marginTop = 10;
+    const marginRight = 10;
+    const marginBottom = 10;
+    const marginLeft = 40;
+
+    // Rows are separated by dx pixels, columns by dy pixels. These names can be counter-intuitive
+    // (dx is a height, and dy a width). This because the tree must be viewed with the root at the
+    // “bottom”, in the data domain. The width of a column is based on the tree’s height.
+    const root = d3.hierarchy(data1);
+    const dx = 10;
+    const dy = (width - marginRight - marginLeft) / (1 + root.height);
+
+    // Define the tree layout and the shape for links.
+    const tree = d3.tree().nodeSize([dx, dy]);
+    const diagonal = d3.linkHorizontal().x(d => d.y).y(d => d.x);
+
+    // Create the SVG container, a layer for the links and a layer for the nodes.
+    const svg = d3.create("svg")
+        .attr("width", width)
+        .attr("height", dx)
+        .attr("viewBox", [-marginLeft, -marginTop, width, dx])
+        .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif; user-select: none;");
+
+    const gLink = svg.append("g")
+        .attr("fill", "none")
+        .attr("stroke", "#555")
+        .attr("stroke-opacity", 0.4)
+        .attr("stroke-width", 1.5);
+
+    const gNode = svg.append("g")
+        .attr("cursor", "pointer")
+        .attr("pointer-events", "all");
+
+    function update(event, source) {
+        const duration = event?.altKey ? 2500 : 250; // hold the alt key to slow down the transition
+        const nodes = root.descendants().reverse();
+        const links = root.links();
+
+        // Compute the new tree layout.
+        tree(root);
+
+        let left = root;
+        let right = root;
+        root.eachBefore(node => {
+        if (node.x < left.x) left = node;
+        if (node.x > right.x) right = node;
+        });
+
+        const height = right.x - left.x + marginTop + marginBottom;
+
+        const transition = svg.transition()
+            .duration(duration)
+            .attr("height", height)
+            .attr("viewBox", [-marginLeft, left.x - marginTop, width, height])
+            .tween("resize", window.ResizeObserver ? null : () => () => svg.dispatch("toggle"));
+
+        // Update the nodes…
+        const node = gNode.selectAll("g")
+        .data(nodes, d => d.id);
+
+        // Enter any new nodes at the parent's previous position.
+        const nodeEnter = node.enter().append("g")
+            .attr("transform", d => `translate(${source.y0},${source.x0})`)
+            .attr("fill-opacity", 0)
+            .attr("stroke-opacity", 0)
+            .on("click", (event, d) => {
+            d.children = d.children ? null : d._children;
+            update(event, d);
+            });
+
+        nodeEnter.append("circle")
+            .attr("r", 2.5)
+            .attr("fill", d => d._children ? "#555" : "#999")
+            .attr("stroke-width", 10);
+
+        nodeEnter.append("text")
+            .attr("dy", "0.31em")
+            .attr("x", d => d._children ? -6 : 6)
+            .attr("text-anchor", d => d._children ? "end" : "start")
+            .text(d => d.data.name)
+        .clone(true).lower()
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-width", 3)
+            .attr("stroke", "white");
+
+        // Transition nodes to their new position.
+        const nodeUpdate = node.merge(nodeEnter).transition(transition)
+            .attr("transform", d => `translate(${d.y},${d.x})`)
+            .attr("fill-opacity", 1)
+            .attr("stroke-opacity", 1);
+
+        // Transition exiting nodes to the parent's new position.
+        const nodeExit = node.exit().transition(transition).remove()
+            .attr("transform", d => `translate(${source.y},${source.x})`)
+            .attr("fill-opacity", 0)
+            .attr("stroke-opacity", 0);
+
+        // Update the links…
+        const link = gLink.selectAll("path")
+        .data(links, d => d.target.id);
+
+        // Enter any new links at the parent's previous position.
+        const linkEnter = link.enter().append("path")
+            .attr("d", d => {
+            const o = {x: source.x0, y: source.y0};
+            return diagonal({source: o, target: o});
+            });
+
+        // Transition links to their new position.
+        link.merge(linkEnter).transition(transition)
+            .attr("d", diagonal);
+
+        // Transition exiting nodes to the parent's new position.
+        link.exit().transition(transition).remove()
+            .attr("d", d => {
+            const o = {x: source.x, y: source.y};
+            return diagonal({source: o, target: o});
+            });
+
+        // Stash the old positions for transition.
+        root.eachBefore(d => {
+        d.x0 = d.x;
+        d.y0 = d.y;
+        });
+    }
+
+    // Do the first update to the initial configuration of the tree — where a number of nodes
+    // are open (arbitrarily selected as the root, plus nodes with 7 letters).
+    root.x0 = dy / 2;
+    root.y0 = 0;
+    root.descendants().forEach((d, i) => {
+        d.id = i;
+        d._children = d.children;
+        if (d.depth && d.data.name.length !== 7) d.children = null;
+    });
+
+    update(null, root);
+
+    return svg.node();
+}
+
+function initD3b() {
+    const format = d3.format(",");
+    const nodeSize = 25;
+    const root = d3.hierarchy(data1).eachBefore((i => d => d.index = i++)(0));
+    const nodes = root.descendants();
+    const width = 100;
+    const height = (nodes.length + 1) * nodeSize;
+  
+    const columns = [
+      {
+        label: "Size", 
+        value: d => d.value, 
+        format, 
+        x: 280
+      },
+      {
+        label: "Count", 
+        value: d => d.children ? 0 : 1, 
+        format: (value, d) => d.children ? format(value) : "-", 
+        x: 340
+      }
+    ];
+  
+    const svg = d3.create("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [-nodeSize / 2, -nodeSize * 3 / 2, width, height])
+        .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif; overflow: visible;");
+  
+    const link = svg.append("g")
+        .attr("fill", "none")
+        .attr("stroke", "#999")
+      .selectAll()
+      .data(root.links())
+      .join("path")
+        .attr("d", d => `
+          M${d.source.depth * nodeSize},${d.source.index * nodeSize}
+          V${d.target.index * nodeSize}
+          h${nodeSize}
+        `);
+  
+    const node = svg.append("g")
+      .selectAll()
+      .data(nodes)
+      .join("g")
+        .attr("transform", d => `translate(0,${d.index * nodeSize})`);
+  
+    node.append("circle")
+        .attr("cx", d => d.depth * nodeSize)
+        .attr("r", 2.5)
+        .attr("fill", d => d.children ? null : "#999");
+  
+    node.append("text")
+        .attr("dy", "0.32em")
+        .attr("x", d => d.depth * nodeSize + 6)
+        .text(d => d.data.name);
+  
+    node.append("title")
+        .text(d => d.ancestors().reverse().map(d => d.data.name).join("/"));
+  
+    for (const {label, value, format, x} of columns) {
+      svg.append("text")
+          .attr("dy", "0.32em")
+          .attr("y", -nodeSize)
+          .attr("x", x)
+          .attr("text-anchor", "end")
+          .attr("font-weight", "bold")
+          .text(label);
+  
+      node.append("text")
+          .attr("dy", "0.32em")
+          .attr("x", x)
+          .attr("text-anchor", "end")
+          .attr("fill", d => d.children ? null : "#555")
+        .data(root.copy().sum(value).descendants())
+          .text(d => format(d.value, d));
+    }
+    return svg.node();
+  }
+  
+  
+  function initD3a() {
+    // Declare the chart dimensions and margins.
+    const width = 640;
+    const height = 400;
+    const marginTop = 20;
+    const marginRight = 20;
+    const marginBottom = 30;
+    const marginLeft = 40;
+  
+    // Declare the x (horizontal position) scale.
+    const x = d3.scaleUtc()
+        .domain([new Date("2023-01-01"), new Date("2024-01-01")])
+        .range([marginLeft, width - marginRight]);
+  
+    // Declare the y (vertical position) scale.
+    const y = d3.scaleLinear()
+        .domain([0, 100])
+        .range([height - marginBottom, marginTop]);
+  
+    // Create the SVG container.
+    const svg = d3.create("svg")
+        .attr("width", width)
+        .attr("height", height);
+  
+    // Add the x-axis.
+    svg.append("g")
+        .attr("transform", `translate(0,${height - marginBottom})`)
+        .call(d3.axisBottom(x));
+  
+    // Add the y-axis.
+    svg.append("g")
+        .attr("transform", `translate(${marginLeft},0)`)
+        .call(d3.axisLeft(y));
+  
+    // Append the SVG element.
+    return svg.node();
+  
+  }
+  
+
+
+
 function getTreeData1() {
     return  {
     "name": "flare",
--- gwidgs/d3/page.js
+++ gwidgs/d3/page.js
@@ -1,6 +1,5 @@
 // let's assume that it's imported in an html file
 var grist;
-var data1 = getTreeData2();
 
 // document.getElementById('container').append(initD3a());
 
@@ -80,10 +79,6 @@ async function gristSelectedRecordChanged(record, mappings) {
 */
 
 
-
-
-
-
 // when some CRUD operation is performed on the table, we want to update the calendar
 async function gristTableChanged(records, mappings) {
   if (mappings) { colTypesFetcher.gotMappings(mappings); }
@@ -94,133 +89,13 @@ async function gristTableChanged(records, mappings) {
     // const CalendarEventObjects = mappedRecords.filter(isRecordValid).map(r => buildCalendarEventObject(r, colTypes));
     // await calendarHandler.updateCalendarEvents(CalendarEventObjects);
     document.getElementById('container').innerHTML = '';
-    document.getElementById('container').append(initD3b());
+    document.getElementById('container').append(initD3c());
   }
   dataVersion = Date.now();
 }
 
 function focusWidget() {
   window.focus();
-}
-
-function initD3b() {
-  const format = d3.format(",");
-  const nodeSize = 25;
-  const root = d3.hierarchy(data1).eachBefore((i => d => d.index = i++)(0));
-  const nodes = root.descendants();
-  const width = 100;
-  const height = (nodes.length + 1) * nodeSize;
-
-  const columns = [
-    {
-      label: "Size", 
-      value: d => d.value, 
-      format, 
-      x: 280
-    },
-    {
-      label: "Count", 
-      value: d => d.children ? 0 : 1, 
-      format: (value, d) => d.children ? format(value) : "-", 
-      x: 340
-    }
-  ];
-
-  const svg = d3.create("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("viewBox", [-nodeSize / 2, -nodeSize * 3 / 2, width, height])
-      .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif; overflow: visible;");
-
-  const link = svg.append("g")
-      .attr("fill", "none")
-      .attr("stroke", "#999")
-    .selectAll()
-    .data(root.links())
-    .join("path")
-      .attr("d", d => `
-        M${d.source.depth * nodeSize},${d.source.index * nodeSize}
-        V${d.target.index * nodeSize}
-        h${nodeSize}
-      `);
-
-  const node = svg.append("g")
-    .selectAll()
-    .data(nodes)
-    .join("g")
-      .attr("transform", d => `translate(0,${d.index * nodeSize})`);
-
-  node.append("circle")
-      .attr("cx", d => d.depth * nodeSize)
-      .attr("r", 2.5)
-      .attr("fill", d => d.children ? null : "#999");
-
-  node.append("text")
-      .attr("dy", "0.32em")
-      .attr("x", d => d.depth * nodeSize + 6)
-      .text(d => d.data.name);
-
-  node.append("title")
-      .text(d => d.ancestors().reverse().map(d => d.data.name).join("/"));
-
-  for (const {label, value, format, x} of columns) {
-    svg.append("text")
-        .attr("dy", "0.32em")
-        .attr("y", -nodeSize)
-        .attr("x", x)
-        .attr("text-anchor", "end")
-        .attr("font-weight", "bold")
-        .text(label);
-
-    node.append("text")
-        .attr("dy", "0.32em")
-        .attr("x", x)
-        .attr("text-anchor", "end")
-        .attr("fill", d => d.children ? null : "#555")
-      .data(root.copy().sum(value).descendants())
-        .text(d => format(d.value, d));
-  }
-  return svg.node();
-}
-
-
-function initD3a() {
-  // Declare the chart dimensions and margins.
-  const width = 640;
-  const height = 400;
-  const marginTop = 20;
-  const marginRight = 20;
-  const marginBottom = 30;
-  const marginLeft = 40;
-
-  // Declare the x (horizontal position) scale.
-  const x = d3.scaleUtc()
-      .domain([new Date("2023-01-01"), new Date("2024-01-01")])
-      .range([marginLeft, width - marginRight]);
-
-  // Declare the y (vertical position) scale.
-  const y = d3.scaleLinear()
-      .domain([0, 100])
-      .range([height - marginBottom, marginTop]);
-
-  // Create the SVG container.
-  const svg = d3.create("svg")
-      .attr("width", width)
-      .attr("height", height);
-
-  // Add the x-axis.
-  svg.append("g")
-      .attr("transform", `translate(0,${height - marginBottom})`)
-      .call(d3.axisBottom(x));
-
-  // Add the y-axis.
-  svg.append("g")
-      .attr("transform", `translate(${marginLeft},0)`)
-      .call(d3.axisLeft(y));
-
-  // Append the SVG element.
-  return svg.node();
-
 }
 
 // We have no good way yet to get the type of a mapped column when multiple types are allowed. We
