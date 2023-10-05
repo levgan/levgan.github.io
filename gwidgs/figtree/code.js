95b5a68ff03e62508af7da4ec2d6c42a2affa4ce
levy <heylevy@gmail.com>
2023-10-05 11:57:18 -0400

    figtree

---
+++ gwidgs/figtree/code.js
@@ -0,0 +1,30 @@
+import forkMe from 'fork-me-github'
+
+import { Tree } from 'tree-master/src/tree'
+
+const data = {
+    children: [
+        { name: 'furity', children: [
+            { name: 'apples', children: [] },
+            { name: 'oranges', children: [
+                { name: 'tangerines', children: [] },
+                { name: 'mandarins', children: [] },
+                { name: 'pomelo', children: [] },
+                { name: 'blood orange', children: [] },
+            ] }
+        ]},
+        { name: 'vegetabley', children: [
+            { name: 'brocolli', children: [] },
+        ] },
+    ]
+}
+
+function test() {
+    const tree = new Tree(data, { parent: document.body })
+    tree.expandAll()
+}
+
+window.onload = function () {
+    test()
+    forkMe('https://github.com/davidfig/tree')
+}
