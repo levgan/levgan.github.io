f0d276ffb8ec8b998e1d4fd32c28a7749d55ffe1
levy <heylevy@gmail.com>
2023-10-09 09:26:05 -0400

    test

--- gwidgs/figtree/code.js
+++ gwidgs/figtree/code.js
@@ -21,23 +21,33 @@ function test() {
 
     var elements = document.getElementsByClassName("yy-tree");
 
-    var myFunction = function() {
-        // var attribute = this.getAttribute("data-myattribute");
-        // alert(attribute);
-        console.log('clicked');
-    };
-    
     for (var i = 0; i < elements.length; i++) {
-        elements[i].addEventListener('clicked', myFunction, false);
+        elements[i].addEventListener('clicked', treeClicked, false);
+        alert('*******************event listener added');
+        //elements[i].addEventListener('click', myFunction, false);
+    }
+
+    var elements = document.getElementsByClassName("yy-tree-name");
+
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].addEventListener('clicked', treeNameClicked, false);
         alert('*******************event listener added');
         //elements[i].addEventListener('click', myFunction, false);
     }
 
 }
 
-function treeClicked(e) {
-    console.log('treeClicked');
-}
+var treeClicked = function() {
+    // var attribute = this.getAttribute("data-myattribute");
+    // alert(attribute);
+    console.log('clicked');
+};
+
+var treeNameClicked = function() {
+    // var attribute = this.getAttribute("data-myattribute");
+    // alert(attribute);
+    console.log('name clicked');
+};
 
 window.onload = function () {
     test()
